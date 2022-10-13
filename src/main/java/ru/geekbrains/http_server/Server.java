9e98e7b9edf724f02df822b459ed07460bd17dc2
gambino <lightsan52rus@yandex.ru>
2022-10-13 10:32:40 +0300

    ASD

--- src/main/java/ru/geekbrains/http_server/Server.java
+++ src/main/java/ru/geekbrains/http_server/Server.java
@@ -1,12 +1,11 @@
 package ru.geekbrains.http_server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Server {
 
@@ -17,26 +16,43 @@ public class Server {
             for(;;) {
                 Socket socket = serverSocket.accept();
                 System.out.println("Client connected");
+
                 BufferedReader reader = new BufferedReader(
                         new InputStreamReader(
                                 socket.getInputStream(), StandardCharsets.UTF_8));
+
 
                 PrintWriter writer = new PrintWriter(
                         socket.getOutputStream());
 
                 while (!reader.ready()) ;
+                String [] a = reader.readLine().split("/");
+                String [] b= a[1].split(" ");
+                File stroke = new File("C:\\Users\\Gambino\\Desktop\\geek-base-oop\\www");
+                FileReader fr = new FileReader("C:\\Users\\Gambino\\Desktop\\geek-base-oop\\www\\index.html");
+                for (File file: Objects.requireNonNull(stroke.listFiles())) {
+                    if (b[0].equals(file.getName())){
+                        writer.println("HTTP/1.1 200 OK");
+                        writer.println("Content-Type: text/html; charset=utf-8");
+                        writer.println();
+                        writer.println("<h1>Hello betina </h1>");
+                        writer.flush();
+                        writer.println();
+                        socket.close();
+                    }
+                    else {
+                        System.out.println(file.getName());
+                        writer.println("HTTP/1.1 404 OK");
+                        writer.println("Content-Type: text/html; charset=utf-8");
+                        writer.println();
+                        writer.println("<h1>404 NOT FOUND </h1>");
+                        writer.flush();
+                        writer.println();
+                        socket.close();
+                    }
 
-                while (reader.ready()) {
-                    System.out.println(reader.readLine());
                 }
 
-                writer.println("HTTP/1.1 200 OK");
-                writer.println("Content-Type: text/html; charset=utf-8");
-                writer.println();
-                writer.println("<h1>Hello world!!!</h1>");
-                writer.flush();
-
-                socket.close();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
